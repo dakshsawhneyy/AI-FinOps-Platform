d0d61f61c019c0f1e3fc0f8a529686872b14dd1b
Daksh Sawhney <darkstardaksh@gmail.com>
2025-10-28 00:16:28 +0530

    updated kafka-cluster config [latest strimzi v - UseKRaft

--- terraform/kafka.tf
+++ terraform/kafka.tf
@@ -1,14 +1,12 @@
-# Wait for helm to install strimzi chart, and then start creating CRD resources
-resource "time_sleep" "wait_for_strimzi_crds" {
+resource "time_sleep" "wait_for_strimzi" {
   depends_on = [ helm_release.strimzi ]
-
   create_duration = "30s"
 }
 
 # Kafka Cluster
 resource "kubernetes_manifest" "kafka_cluster" {
   # provider = kubernetes.eks_cluster
-  depends_on = [ time_sleep.wait_for_strimzi_crds ]
+  depends_on = [ time_sleep.wait_for_strimzi ]
 
   manifest = {
     "apiVersion" = "kafka.strimzi.io/v1beta2"
@@ -20,41 +18,44 @@ resource "kubernetes_manifest" "kafka_cluster" {
     "spec" = {
         "kafka" = {
             "version" = "4.0.0"
-            "replicas" = 3   # 3 Worker Pods for kafka
+            "nodePools" = [{
+                "name" = "kafka" # Pool name (must exist, named 'kafka' here)
+                "roles" = [
+                    "broker",    # Act as brokers
+                    "controller" # Act as KRaft controllers
+                ]
+                "replicas" = 3       # 3 nodes in this pool
+                "storage" = {
+                    "type"        = "persistent-claim" # Use persistent storage
+                    "size"        = "20Gi"
+                    "deleteClaim" = true
+                }
+            }]
             # we didn't specified how producer consumer connect to kafka broker -- listeners does this task
             "listeners" = [{
                 "name" = "plain" # Give the listener a name
                 "port" = 9092     # Standard Kafka port
                 "type" = "internal" # Only accessible within the Kubernetes cluster
                 "tls"  = false    # Disable TLS encryption for this internal listener
+            },{
+                "name" = "controller" 
+                "port" = 9093     
+                "type" = "internal" 
+                "tls"  = false    
             }]
-            "storage" = {
-                "type" = "jbod"   # Just a Bunch of Disks.
-                "volumes" = [{
-                    "id" = 0
-                    "type" = "persistent-claim"    # Go ask aws for 20GB Persistent Volume (EBS)
-                    "size" = "20Gi"
-                    "deleteClaim" = true   # If i delete this kafka cluster, delete ebs as well
-                }]
-            }
         }
-        # ZooKeeper manages kafka's worker nodes
-        "zookeeper" = {
-            "replicas" = 3
-            "storage" = {
-                "type"= "persistent-claim"
-                "size"= "10Gi" # 10GB disk for each zookeeper
-                "deleteClaim"=  true
-            }
+        "featureGates" = {
+            "UseKRaft" = "Enabled"
         }
     }
   }
 }
 
+
 # Kafka Topic
 resource "kubernetes_manifest" "kafka-topic-aicosts" {
   # provider = kubernetes.eks_cluster
-  depends_on = [ time_sleep.wait_for_strimzi_crds ]
+  depends_on = [ kubernetes_manifest.kafka_cluster ]
 
   manifest = {
     "kind": "KafkaTopic"
